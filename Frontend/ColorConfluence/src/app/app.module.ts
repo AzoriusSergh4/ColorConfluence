c10b9cd3eb2cf402a9c17a87d70558a976966cfb
AzoriusSergh4 <43406514+AzoriusSergh4@users.noreply.github.com>
2019-09-23 08:14:21 +0200

    dependencias añadidas

--- Frontend/ColorConfluence/src/app/app.module.ts
+++ Frontend/ColorConfluence/src/app/app.module.ts
@@ -9,6 +9,21 @@ import { BuscadorComponent } from './components/buscador/buscador.component';
 import { CalculadoraManaComponent } from './components/calculadora-mana/calculadora-mana.component';
 import { GestorMazosComponent } from './components/gestor-mazos/gestor-mazos.component';
 import { ErrorComponent } from './components/error/error/error.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { CovalentLayoutModule } from '@covalent/core/layout';
+import { CovalentStepsModule  } from '@covalent/core/steps';
+/* any other core modules */
+// (optional) Additional Covalent Modules imports
+import { CovalentHttpModule } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
@@ -19,10 +34,23 @@ import { ErrorComponent } from './components/error/error/error.component';
     CalculadoraManaComponent,
     GestorMazosComponent,
     ErrorComponent
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CovalentLayoutModule,
+    CovalentStepsModule,
+    // (optional) Additional Covalent Modules imports
+    CovalentHttpModule.forRoot(),
+    CovalentHighlightModule,
+    CovalentMarkdownModule,
+    CovalentDynamicFormsModule,
+    MatButtonModule, MatCheckboxModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
