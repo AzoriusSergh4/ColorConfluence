f6c183edc8c65b54d69fc05283779288ccdef650
AzoriusSergh4 <43406514+AzoriusSergh4@users.noreply.github.com>
2019-10-18 10:43:58 +0200

    Backend correcto

---
+++ Backend/src/main/java/com/cc/ColorConfluenceApplication.java
@@ -0,0 +1,13 @@
+package com.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColorConfluenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ColorConfluenceApplication.class, args);
+	}
+
+}
